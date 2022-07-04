ca47e83e3acda17bcaafe2a480f1c842b4147f9c
pengfei.zhou <pengfeizhou@foxmail.com>
2022-07-04 16:30:10 +0800

    feat: add example

--- example/src/Example.tsx
+++ example/src/Example.tsx
@@ -1,36 +1,133 @@
-import { Text, Group, Panel, VLayout, layoutConfig, Gravity, jsx } from "doric";
-import { action, makeObservable, observable } from "mobx";
+import {
+  Text,
+  List,
+  Group,
+  Panel,
+  layoutConfig,
+  Gravity,
+  jsx,
+  HLayout,
+  Stack,
+  ListItem,
+  Color,
+  modal,
+  GestureContainer,
+  createRef,
+} from "doric";
+import { action, computed, makeObservable, observable } from "mobx";
 import { observer } from "mobx-doric";
 
-class Ticker {
+interface ToDoItem {
+  title: string;
+  checked: boolean;
+  deadline: number;
+}
+
+class ToDoList {
   constructor() {
     makeObservable(this);
   }
 
-  @observable tick = 0;
+  @observable todos: ToDoItem[] = [];
+
+  @computed get allLength() {
+    return this.todos.length;
+  }
 
   @action.bound
-  increase() {
-    this.tick++;
+  add(todo: ToDoItem) {
+    this.todos.push(todo);
   }
 }
 
-@Entry
-export class CounterPanel extends Panel {
-  ticker = new Ticker();
-  build(root: Group) {
-    <VLayout
-      parent={root}
-      layoutConfig={layoutConfig().most()}
-      gravity={Gravity.Center}
-      space={20}
+function AddButton(props: { onClick?: () => void }) {
+  const textRef = createRef<Text>();
+  return (
+    <GestureContainer
+      onTouchDown={() => {
+        textRef.current.backgroundColor = Color.parse("#2980b9");
+      }}
+      onTouchCancel={() => {
+        textRef.current.backgroundColor = Color.parse("#3498db");
+      }}
+      onTouchUp={() => {
+        textRef.current.backgroundColor = Color.parse("#3498db");
+      }}
+      onSingleTap={props.onClick}
+      layoutConfig={layoutConfig()
+        .fit()
+        .configAlignment(Gravity.Right.bottom())
+        .configMargin({ bottom: 30, right: 30 })}
     >
-      {observer(() => (
-        <Text textSize={40}>{`${this.ticker.tick}`}</Text>
-      ))}
-      <Text textSize={20} onClick={this.ticker.increase}>
-        Increase
+      <Text
+        ref={textRef}
+        width={50}
+        layoutConfig={layoutConfig().just()}
+        height={50}
+        corners={25}
+        backgroundColor={Color.parse("#3498db")}
+        textColor={Color.WHITE}
+        textSize={30}
+        textAlignment={Gravity.Center}
+      >
+        +
       </Text>
-    </VLayout>;
+    </GestureContainer>
+  );
+}
+
+function ToDoCell(props: { item: ToDoItem }) {
+  return observer(() => (
+    <ListItem layoutConfig={layoutConfig().mostWidth().fitHeight()}>
+      <HLayout
+        layoutConfig={layoutConfig()
+          .mostWidth()
+          .justHeight()
+          .configMargin({ bottom: 1 })}
+        height={50}
+        gravity={Gravity.CenterY}
+        backgroundColor={
+          props.item.checked ? Color.parse("#2ecc71") : Color.parse("#e67e22")
+        }
+        onClick={() => {
+          props.item.checked = true;
+        }}
+        padding={{ left: 20, right: 20 }}
+      >
+        <Text textSize={20} textColor={Color.WHITE}>
+          {`${props.item.checked ? "☑︎" : "☐"}\t${props.item.title}`}
+        </Text>
+      </HLayout>
+    </ListItem>
+  ));
+}
+
+@Entry
+export class ToDoListPanel extends Panel {
+  todos = new ToDoList();
+  build(root: Group) {
+    <Stack layoutConfig={layoutConfig().most()} parent={root}>
+      {observer(() => (
+        <List
+          layoutConfig={layoutConfig().most()}
+          itemCount={this.todos.allLength}
+          renderItem={(idx) =>
+            (<ToDoCell item={this.todos.todos[idx]} />) as ListItem
+          }
+        />
+      ))}
+      <AddButton
+        onClick={async () => {
+          const text = await modal(this.context).prompt({
+            title: "Please input todo",
+          });
+          this.todos.add({
+            title: text,
+            checked: false,
+            deadline: 0,
+          });
+        }}
+      />
+    </Stack>;
   }
 }
